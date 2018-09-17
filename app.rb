a046fc6152c861533b54812169d65ea59950f780
Naeblis <johnmlowery@gmail.com>
2018-09-17 12:21:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Naeblis"
+    end
+
+    get '/hometown' do
+        "My hometown is Kansas City"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Muthor"
+    end
 end
